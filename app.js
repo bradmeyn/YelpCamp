ac895f99bd3d5cb41ab4da5e64831c7ccc602858
Brad Meyn <bradjmeyn@gmail.com>
2021-06-04 09:33:20 +1000

    add additional pages to display all campgrounds, individual campgrouds and create new campgrounds

--- app.js
+++ app.js
@@ -19,28 +19,46 @@ db.once('open', () => {
     console.log("database connected");
 });
 
-
+//use express
 const app = express();
 
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+//add ability to parse body
+app.use(express.urlencoded({extended:true}));
 
 
 app.get("/", (req, res) =>{
     res.render('home')
 });
 
-
-app.get("/makecampground", async (req, res) =>{
-    const camp = new Campground({
-        title: 'my backyard',
-        description: "cheap camping"
-    });
-    await camp.save();
-    res.send(camp)
+//display all campgrounds and pass through all campgrounds from database
+app.get("/campgrounds", async (req, res) =>{
+    const campgrounds = await Campground.find();
+    res.render('campgrounds/index', {campgrounds});
 });
+
+//create a new campground with form
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+
+});
+
+//post request to /campgrounds for creation of new campground
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+});
+
+//find and display single campground searching db by id
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground});
+});
+
 
 
 
