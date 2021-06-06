4d0f59ad641afcbafff430541f9edabe501e8e13
Brad Meyn <bradjmeyn@gmail.com>
2021-06-06 17:28:17 +1000

    finish basic styling and crud functionality

--- seeds/index.js
+++ seeds/index.js
@@ -32,9 +32,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: 'https://source.unsplash.com/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus purus lorem, et blandit libero mattis in. Cras lobortis magna lorem, sit amet blandit purus fringilla vel. Donec blandit aliquet ipsum ac mattis. Sed vehicula nisl justo, sit amet blandit mi eleifend eu. Proin vitae tempor ipsum. Nulla facilisi. Aenean auctor lectus ligula, ac finibus neque accumsan hendrerit. Nullam vel congue lacus, a scelerisque massa. Aenean blandit, magna sit amet semper ullamcorper, erat enim luctus nulla, sit amet pulvinar est risus nec urna. Quisque mauris massa, semper sed fermentum vitae, congue aliquet odio. Nullam lectus ipsum, rhoncus id lectus vitae, porttitor pretium nunc. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis a volutpat felis.',
+            price: price
         });
         await camp.save();
     }
