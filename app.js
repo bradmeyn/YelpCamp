1bbd1c7f475499583eef335702ecd08f9ee7f18d
Brad Meyn <bradjmeyn@gmail.com>
2021-06-09 11:38:22 +1000

    add ability to create and show review

--- app.js
+++ app.js
@@ -86,7 +86,8 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 
 //find and display single campground searching db by id
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    console.log(campground)
     res.render('campgrounds/show', {campground});
 }));
 
