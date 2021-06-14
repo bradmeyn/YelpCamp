0874b6ff0e5909b0765c10ea54f1538591d4d624
Brad Meyn <bradjmeyn@gmail.com>
2021-06-14 10:33:17 +1000

    add routes page for reviews

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ const path = require('path');
 
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-
-const {campgroundSchema, reviewSchema} = require('./schemas.js');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+
+
 const methodOverride = require('method-override');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
+
 
 const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
 
 //connect mongoose to mongodb server
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -42,18 +41,9 @@ app.use(methodOverride('_method'));
 
 
 
-const validateReview = (req, res, next) => {
-    const {error} =reviewSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
-
-//use campgrounds route
+//use router files with defined prefixes
 app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
 
 
 
@@ -65,24 +55,6 @@ app.get("/", (req, res) =>{
 
 
 
-
-//post route for review
-app.post('/campgrounds/:id/reviews',validateReview, catchAsync(async(req, res)=> {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) =>{
-    const {id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(req.params.reviewId);
-    res.redirect(`/campgrounds/${id}`)
-
-}));
 
 app.all('*', (req, res, next) =>{
     next( new ExpressError('Page not found', 404))
