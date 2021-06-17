3667c812b14ea8b60db9c139f88bbd9d673ee0fe
Brad Meyn <bradjmeyn@gmail.com>
2021-06-17 20:39:23 +1000

    add register page

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
+
 const express = require('express');
-//use express
-const app = express();
 const path = require('path');
-const flash = require('connect-flash');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-//override post routes for form submission
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users')
 
-
-//connect mongoose to mongodb server
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -24,25 +22,24 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useFindAndModify: false
 });
 
-
-//confirm database connection
 const db = mongoose.connection;
-db.on('error',console.error.bind('connection error:'));
-db.once('open', () => {
-    console.log("database connected");
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
 });
 
+const app = express();
 
-app.engine('ejs', ejsMate);
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
 
-//add ability to parse body
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
 
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret',
+    secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -51,41 +48,44 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
-
-app.use(session(sessionConfig));
-
+app.use(session(sessionConfig))
 app.use(flash());
 
 app.use((req, res, next) => {
-    res.locals.error = req.flash('error');
     res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
     next();
-});
+})
 
-//routes files
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+app.use('/', userRoutes)
 
-//use router files with defined prefixes
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
-
-app.get("/", (req, res) =>{
+app.get('/', (req, res) => {
     res.render('home')
 });
 
-
-
-app.all('*', (req, res, next) =>{
-    next( new ExpressError('Page not found', 404))
+app.get('/fakeUser', async(req, res) =>{
+    const user = new User({email: 'brad@mail.com', username: 'brad'});
+    const newUser = await User.register(user, 'chicken');
+    res.send(newUser)
 });
 
-app.use((err, req, res, next)=> {
-    const {statusCode = 500} = err;
-    if(!err.message) err.message = 'something went wrong'
-    res.status(statusCode).render('error', {err});
-   
-});
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
+
 app.listen(3000, () => {
-   console.log('server running on port 3000')
-});
+    console.log('Serving on port 3000')
+})
+
+
+
+
