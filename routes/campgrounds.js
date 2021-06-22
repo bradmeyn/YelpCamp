9f5ca43477038917fa559d9c69010b242726f43a
Brad Meyn <bradjmeyn@gmail.com>
2021-06-22 10:41:59 +1000

    block users from editing other campgrounds not created by them

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
-const Campground = require('../models/campground');
-const {campgroundSchema} = require('../schemas.js');
-const {isLoggedIn} = require('../middleware');
 
-const validateCampground = (req, res, next) => {
-    console.log(req.body);
-    const {error} = campgroundSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+const Campground = require('../models/campground');
+
+const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
+
+
 
 //display all campgrounds and pass through all campgrounds from database
 router.get("/", catchAsync(async (req, res) =>{
@@ -42,7 +33,7 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 //find and display single campground searching db by id
 router.get('/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
-    console.log(campground);
+
     if(!campground){
         req.flash('error', 'Cannot find that campground');
         return res.redirect('/campgrounds')
@@ -51,31 +42,36 @@ router.get('/:id', catchAsync(async (req, res) => {
 }));
 
 //serve form to update an existing campground
-router.get('/:id/edit', isLoggedIn, async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+router.get('/:id/edit', isLoggedIn,isAuthor, async (req, res) => {
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
 
     if(!campground){
         req.flash('error', 'Cannot find that campground');
         return res.redirect('/campgrounds')
     }
-    
+
     res.render('campgrounds/edit', {campground});
 });
 
 //edit single campground
-router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
-    const { id} = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground });
+router.put('/:id', isLoggedIn,isAuthor, validateCampground, catchAsync(async (req, res) => {
+  
+    const {id} = req.params;
+    
+   const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground });
   req.flash('success','Camground successfully updated');
   res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 //delete route for single campground
-router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
-    const {id} = req.params;
-  const campground = await Campground.findByIdAndDelete(id, {...req.body.campground });
-  req.flash('success','Campground deleted');
-  res.redirect('/campgrounds');
+router.delete('/:id', isLoggedIn,isAuthor, catchAsync(async (req, res) => {
+
+const {id} = req.params;
+  
+const campground = await Campground.findByIdAndDelete(id, {...req.body.campground });
+req.flash('success','Campground deleted');
+res.redirect('/campgrounds');
 }));
 
 
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,24 +3,15 @@ const router = express.Router({mergeParams: true});
 
 const Campground = require('../models/campground');
 const Review = require('../models/review');
+const {validateReview} = require('../middleware');
 
 
-//review schema for joi validation
-const {reviewSchema} = require('../schemas.js');
 
 const catchAsync = require('../utils/catchAsync');
 
 const ExpressError = require('../utils/ExpressError');
 
-const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+
 
 //post route for review
 router.post('/',validateReview, catchAsync(async(req, res)=> {
